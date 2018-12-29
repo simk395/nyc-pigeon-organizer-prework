3942b5bcdeb4e18940c087b261c9345ee702ec5c
Simon Kong <simon.kong95@gmail.com>
2018-12-29 04:20:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon = {}
+  bird_names = data[:gender].values.flatten
+  bird_names.each do |name|
+    pigeon[name] = {}
+  end
+  data.each do |info, group_data|
+    group_data.each do |bio, name|
+      name.each do |bird|
+        if pigeon[bird].has_key?(info)
+          pigeon[bird][info] << bio.to_s
+        else
+          pigeon[bird][info] = [bio.to_s]
+        end
+      end
+    end
+  end
+  pigeon
 end
